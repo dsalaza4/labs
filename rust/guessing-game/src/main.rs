dd40cabe0916a10e17f42cef7ac3725793a317a6
Daniel Salazar <podany270895@gmail.com>
2025-11-28 20:43:09 -0500

    feat: guessing-game
    - Finish guessing-game

--- rust/guessing-game/src/main.rs
+++ rust/guessing-game/src/main.rs
@@ -1,15 +1,38 @@
+use std::cmp::Ordering;
 use std::io;
+use std::num;
+
+use rand::Rng;
+
+fn get_user_guess() -> Result<u32, num::ParseIntError> {
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    return guess.trim().parse();
+}
 
 fn main() {
     println!("Guess the number!");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
     println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {guess}");
+    loop {
+        let guess = match get_user_guess() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            }
+        };
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
